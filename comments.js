94b95156890fb0fbb446876b06fc7c3fc680cce6
RobbyFebryan <155985628+RobbyFebryan@users.noreply.github.com>
2025-03-15 06:51:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,24 +1,22 @@
 //Create Web Server
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('Hello World');
-        res.end();
-    }
-
-    if (req.url === '/api/comments') {
-        fs.readFile('comments.json', 'utf8', (err, data) => {
-            if (err) {
-                res.write('Error');
-                res.end();
-            } else {
-                res.write(data);
-                res.end();
-            }
-        });
-    }
+//Create a route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-server.listen(3000);
+//Create a route
+app.get('/comments', (req, res) => {
+  res.send('This is a page with comments');
+});
+
+//Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//Run the server: node comments.js
+//Open a browser and go to http://localhost:3000/commentsgit a
